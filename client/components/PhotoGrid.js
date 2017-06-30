2ecac87b5cee429b3ae1f08304406b91c7988d61
samblacklock <samblacklock@me.com>
2017-06-30 17:30:05 +0100

    finished

---
+++ client/components/PhotoGrid.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Photo from './Photo';
+
+class PhotoGrid extends React.Component {
+  render() {
+    return (
+      <div className="photo-grid">
+        { this.props.posts.map((post, i) => <Photo key={i} i={i} post={post} {...this.props}/>) }
+      </div>
+    )
+  }
+}
+
+
+export default PhotoGrid;
